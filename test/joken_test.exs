8ae409d7cd81a02d09f2dd288ed9b2e6761a0a9a
Alexander Rozumii <alexander.rozumii@toptal.com>
2016-11-23 12:32:57 +0200

    Fix compile-time warning about :random module

--- test/joken_test.exs
+++ test/joken_test.exs
@@ -280,7 +280,7 @@ defmodule Joken.Test do
 
     token = %{}
     |> token
-    |> with_claim_generator("my_claim", fn -> "Random: #{inspect :random.uniform}" end)
+    |> with_claim_generator("my_claim", fn -> "Random: #{inspect :rand.uniform}" end)
 
     claims1 = token
     |> sign(hs256("secret"))
